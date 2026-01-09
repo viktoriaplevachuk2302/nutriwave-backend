49ce66d6939e9e89f4f0c13151098ba01b38b666
viktoriaplevachuk2302 <viktoriaplevachuk5@gmail.com>
2026-01-09 03:47:39 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const app = require('./app.js');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 NutriWave backend running on http://0.0.0.0:${PORT}`);
   console.log(`Health check: http://0.0.0.0:${PORT}/health`);
